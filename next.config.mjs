5f3f493ba62d8422cb9f56a1a52f77cef7f39969
chandu539 <chchandu554@gmail.com>
2025-05-19 19:49:13 +0530

    modify the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.mjs
+export default {
   images: {
-    domains: ['cdn.sanity.io'], // ✅ Allow images from Sanity CDN
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
   },
 };
-
-export default nextConfig;
