e8bf74f7559f42733a8cdf2257dfbe7d62348cf6
chandu539 <chchandu554@gmail.com>
2025-05-29 15:58:25 +0530

    add the measurement id in app.js file google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,44 @@
 import "../styles/globals.css";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag === "function") {
+        window.gtag("config", GA_MEASUREMENT_ID, {
+          page_path: url,
+        });
+      }
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="gtag-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `}
+          </Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
